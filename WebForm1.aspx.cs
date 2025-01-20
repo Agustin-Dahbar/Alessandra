dd899485e4b9917308669c8acf7903ee403b7855
Agustin Dahbar <agustindahbarpp@hotmail.com>
2025-01-20 21:37:17 +0100

    Fin en pc

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CumpleAlessandra
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void torta_Click(object sender, EventArgs e)
+        {
+            lblCompleanno.Visible = true;
+            titulo.Visible = false;
+        }
+    }
+}
